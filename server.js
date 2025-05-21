35bc20540d032ab04de0db35bb2744086db02abd
Mussa Sabimana <nsamussa1@gmail.com>
2025-05-21 14:43:19 +0200

    hamw nshya

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
+
+app.use("*", (req, res) => { res.sendFile(path.resolve(__dirname, "public", 'index.html'))});
 
 // Start the server
 app.listen(PORT, () => {
