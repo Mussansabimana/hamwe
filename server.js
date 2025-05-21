8ac353282ce4c1c0b64aa4daea369ecbf83743c9
Mussa Sabimana <nsamussa1@gmail.com>
2025-05-21 14:51:36 +0200

    hamw nshya

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const path = require('path');
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-app.use("*", (req, res) => { res.sendFile(path.resolve(__dirname, "public", 'index.html'))});
+// Fallback to index.html for any route (like for SPA routing)
+app.use(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
 
 // Start the server
 app.listen(PORT, () => {
